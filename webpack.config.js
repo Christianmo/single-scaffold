737b0d2577ac4bb85abf8caaa584519cb538bfc6
Christian Montenegro <christian.montenegro@tektonlabs.com>
2018-04-06 14:51:37 -0500

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    context: __dirname + "/assets/js",
-    entry: './script.js',
-    output: {
-        path: __dirname + "/statics/js",
-        filename: 'script.js'
-    },
-    module: {
-        rules: [
-            {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-            }
-        ]
-    },
-    devServer : {
-        host: '0.0.0.0',
-        port: '3000',
-        inline: true
-    },
-}
+  context: `${__dirname  }/assets/js`,
+  entry: ['babel-polyfill', './script.js'],
+  output: {
+    path: `${__dirname  }/statics/js`,
+    filename: 'script.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  devServer: {
+    host: '0.0.0.0',
+    port: '3000',
+    inline: true,
+  },
+};
